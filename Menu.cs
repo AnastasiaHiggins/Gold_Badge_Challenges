fa5e26b2c6369b9165ddab5c4f088d373861b16c
AnastasiaHiggins <higgins.ana@gmail.com>
2020-10-28 18:24:27 -0400

    initial commit

---
+++ Menu.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Cafe_ClassLibrary
+{
+    public class MenuItem
+    {
+
+        public int Number { get; set; }
+        public string Name { get; set; }
+        public string Descriptor { get; set; }
+        public string Ingredients { get; set; }
+        public decimal Price { get; set; }
+
+        //build a constructor!!!
+
+
+
+        //A meal number, so customers can say "I'll have the #5"
+        //A meal name
+        //A description
+        //A list of ingredients,
+        //A price
+
+
+        //Create a Menu Class with properties, constructors, and fields.
+        //Create a MenuRepository Class that has methods needed.
+        //Create a Test Class for your repository methods. (You don't need to test your constructors or objects, just your methods)
+        //Create a Program file that allows the cafe manager to add, delete, and see all items in the menu list.
+    }
+}
---
+++ Menu_Repo.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cafe_ClassLibrary
+{
+    class Menu_Repo
+    {
+        ////List that Points to Your Object By Name
+        //private List<Menu> _menu = new List<Menu>();
+        ////C--Create
+        ////R--Read
+        ////U--Update
+        ////D--Delete
+        ////CREATE
+        //public bool AddMenu(Menu newMenu)
+        //{
+        //    int startingCount = _menu.Count;
+        //    _menu.Add(newMenu);
+        //    bool wasAdded = (_menu.Count > startingCount) ? true : false;
+        //    return wasAdded;
+        //}
+        ////READ ALL
+        //public List<Menu> GetMenu()
+        //{
+        //    return _menu;
+        //}
+        ////READ A SINGLE OBJECT
+        //public Menu GetMenuByName(string name)
+        //{
+        //    //Goes through each object in the list
+        //    foreach (Menu menu in _menu)
+        //    {
+        //        //Checks for a name match with the parameter entered
+        //        if (menu.MealName == name)
+        //        {
+        //            //if the name is found,here is where it is returned.
+        //            return menu;
+        //        }
+        //    }
+        //    return null;
+        //}
+        ////Update an existing Object
+        //public bool UpdateExistingMenu(string originalName, Menu newObject)
+        //{
+        //    return true;
+        //}
+        //public bool DeleteExistingMenu(Menu existingmenu)
+        //{
+        //    //Sets deleteResult to True if the object is removed.
+        //    bool deleteResult = _menu.Remove(existingmenu);
+        //    return deleteResult;
+        //}
+        ////A method for printing entire menu
+    }
+}
